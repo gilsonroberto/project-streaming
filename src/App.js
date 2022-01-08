8e7ab74475a54ee7cfe3692335db06343c5acde0
gilson.souza <gilson.souza@ltm.digital>
2022-01-08 00:21:35 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import './App.css'
+import tmdb from "./tmdb";
+import MovieRow from "./components/MovieRow";
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await tmdb.getHomeList();
+      setMovieList(list);
+      console.log(list)
+    }
+
+    loadAll();
+  }, [])
+
+  return (
+    <div className='page'>
+      <section className='lists'>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
+}
