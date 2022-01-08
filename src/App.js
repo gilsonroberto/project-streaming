9dfa6a6c5b5aa0163242ad94a212d1ecac9bda27
gilson.souza <gilson.souza@ltm.digital>
2022-01-08 03:06:32 -0300

    Inclusao menu principal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import './App.css'
-import tmdb from "./tmdb";
-import MovieRow from "./components/MovieRow";
+import tmdb from './tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie'
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
       let list = await tmdb.getHomeList();
       setMovieList(list);
-      console.log(list)
+      // console.log(list)
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(chosenInfo)
+      console.log(chosen);
+      console.log(chosenInfo);
     }
 
     loadAll();
@@ -19,6 +28,9 @@ export default () => {
 
   return (
     <div className='page'>
+      {featureData &&
+        <FeaturedMovie item={featureData} />
+      }
       <section className='lists'>
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
